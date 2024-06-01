b01eade7ec2091fd785f1bc3b004fa55cec261ef
fgreau <francoisgreau.adressepro@gmail.com>
2024-06-01 20:46:44 +0200

    added Product module, along with products and products-admin components

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from "./product/products/products.component";
+import { ProductsAdminComponent } from "./product/products-admin/products-admin.component";
 
 const routes: Routes = [
-
+  {
+    path: '',
+    children: [
+      { path: 'products', component: ProductsComponent },
+      {
+        path: 'admin',
+        children: [
+          { path: 'products', component: ProductsAdminComponent },
+          { path: '', redirectTo: 'products', pathMatch: 'full' },
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
